b1e42305c25e5ba218e850b3aa66ec1d0aaa9c5e
Ar4mus <Ar4mus@att.net>
2026-06-16 16:04:18 +0330

    Update NipoVPN core from v1.1.58 to v1.1.59

--- src/lib/components/header/Logo.svelte
+++ src/lib/components/header/Logo.svelte
@@ -11,10 +11,10 @@
 				>NipoVPN GUI</span
 			>
 			<span class="mt-1 text-[10px] font-medium bg-primary-300-700 px-1.5 py-0.5 rounded-base"
-				>v0.2.0 beta</span
+				>v0.2.1 beta</span
 			>
 		</div>
 
-		<span class="text-[10px] text-slate-500 dark:text-slate-400">based on nipovpn v1.1.58</span>
+		<span class="text-[10px] text-slate-500 dark:text-slate-400">based on nipovpn v1.1.59</span>
 	</div>
 </div>
